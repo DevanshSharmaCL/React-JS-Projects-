9193328dee85ff5cc6c63bc2947c0fdd996f94f1
DevanshSharmaCL <devanshsharma70cl@gmail.com>
2025-03-01 14:10:57 +0530

    Counter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import Hello_World from "./Projects/Hello_World";
+import Hello_World from "./Projects/Hello_World";
 import Counter from "./Projects/Counter";
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Corrected
 root.render(
     <React.StrictMode>
-       {/* <Hello_World /> */}
-         <Counter />
+        {/* <Hello_World /> */}
+        {/* <Counter /> */}
     </React.StrictMode>
 );
  
