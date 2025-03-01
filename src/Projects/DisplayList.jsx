5944ed8863b317914d136cff09e49cf3449117a3
DevanshSharmaCL <devanshsharma70cl@gmail.com>
2025-03-01 14:32:27 +0530

    Display List

---
+++ src/Projects/DisplayList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function DisplayList(){
+const item =['item1',
+'item2',
+'item3',
+'item4',
+'item5',
+'item6',
+]
+return(
+    <div>
+        <ul>
+            {item.map((item,index)=>{
+                return <li key={index}>{item}</li>
+            })}
+        </ul>
+    </div>
+)}
+
+export default DisplayList
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import Hello_World from "./Projects/Hello_World";
 import Counter from "./Projects/Counter";
 import UserInput from "./Projects/UserInput";
+import DisplayList from "./Projects/DisplayList";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root')); // Corrected
 root.render(
     <React.StrictMode>
         {/* <Hello_World /> */}
         {/* <Counter /> */}
-        <UserInput />
+        {/* <UserInput /> */}
+        <DisplayList />
     </React.StrictMode>
 );
  
