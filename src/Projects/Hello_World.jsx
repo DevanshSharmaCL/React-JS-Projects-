477e9c86767d9dd5af7d458d66c7be7a617c3c19
DevanshSharmaCL <devanshsharma70cl@gmail.com>
2025-03-01 13:48:59 +0530

    Hello World

---
+++ src/Projects/Hello_World.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hello_World = () => {
+  return (
+    <div>Hello_World</div>
+  )
+}
+
+export default Hello_World
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
- 
+// import Hello_World from "./Projects/Hello_World";
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Corrected
 root.render(
     <React.StrictMode>
-       
+       {/* <Hello_World /> */}
     </React.StrictMode>
 );
  
